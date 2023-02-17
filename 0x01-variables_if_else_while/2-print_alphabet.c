134b16f6eee480b311ba313b049e36aebed4e245
Nasankimutai <kimutailetting2020@gmail.com>
2023-02-17 02:41:03 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*main is the entry point
+ *return is always zero for success
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 'a'; b <= 'z'; b++)
+	{
+		putchar(b);
+	}
+	putchar("\n");
+	return (0);
+}
+
+
+	
