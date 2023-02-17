c0fed4bcd7e1bfa4d063ed7f3287a9db6a877093
Nasankimutai <kimutailetting2020@gmail.com>
2023-02-17 03:15:00 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* entry point =main
+ * *return=0 (success)
+ */
+int main()
+{
+	int i;
+	for (i = 0; i ,i <= 9 i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0)
+}
+
